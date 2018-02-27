ea2a05ae5a700117842b5f3c1f2fef6bea98ee41
Stanikov <M.Staniszewski16@gmail.com>
2018-02-27 13:34:33 +0100

    Stanikov
    Add new functionality (Find weather by your ip)

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -74,11 +74,20 @@ public class MainController implements Initializable {
     @FXML
     void checkClick(ActionEvent event) {
     	if(choiceBox.getValue()=="Search by city name") {
-    		String sURL = "http://api.openweathermap.org/data/2.5/weather?q="; //Rutki-Kossaki&APPID=c51c62d65d9b1125d513f20cb5107341";
-    		sURL = new StringBuilder(sURL).append(cityText.getText()).toString();
-    		sURL = new StringBuilder(sURL).append("&APPID=c51c62d65d9b1125d513f20cb5107341").toString();
+    		setWeather(cityText.getText());
+    	}
+    	else if(choiceBox.getValue()=="Search by your location") {
+    		String ip = null;
+    		try {
+				ip = getPublicIp();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}    		
+    		System.out.println(ip);
+    		String sURL = "http://ip-api.com/json/";
+    		sURL = new StringBuilder(sURL).append(ip).toString();
     		System.out.println(sURL);
-    		
     		JsonElement root = null;
 		    try {
 		    	URL url = new URL(sURL);
@@ -92,50 +101,12 @@ public class MainController implements Initializable {
 				alert("Localization not found :(");
 				e.printStackTrace();
 			}
-
-		    //Convert the input stream to a json element
-		    JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object. 
+		    JsonObject rootobj = root.getAsJsonObject();
 		    System.out.println(rootobj.toString());
-		    JsonObject main = rootobj.get("main").getAsJsonObject();
-		    JsonObject windJ = rootobj.get("wind").getAsJsonObject();
-		    JsonArray weather = rootobj.get("weather").getAsJsonArray();
-		    JsonObject weatherr = weather.get(0).getAsJsonObject();
-		    long sunriseUnix = rootobj.get("sys").getAsJsonObject().get("sunrise").getAsLong();
-		    long sunsetUnix = rootobj.get("sys").getAsJsonObject().get("sunset").getAsLong();
-		    long dateUnix = rootobj.get("dt").getAsLong();
-		    Date date = new Date ();
-		    date.setTime((long)sunriseUnix*1000);
-		    sunrise.setText(fixTime(date.toString()));
-		    date.setTime((long)sunsetUnix*1000);
-		    sunset.setText(fixTime(date.toString()));
-		    date.setTime((long)dateUnix*1000);
-		    dateLabel.setText("	" + fixTime(date.toString()));
-		    
-		    String imageUrl = "http://openweathermap.org/img/w/";
-		    imageUrl = new StringBuilder(imageUrl).append(weatherr.get("icon").getAsString()+ ".png").toString();
-		    icon.setImage(new Image(imageUrl));
-		    int temp = main.get("temp").getAsInt();
-		    temp = temp - 273;
-		    top.setText("Weather in " + rootobj.get("name").getAsString()+", "+rootobj.get("sys").getAsJsonObject().get("country").getAsString());
-		    tempLabel.setText(Integer.valueOf(temp).toString() + " C");
-		    weatherLabel.setText(weatherr.get("main").getAsString());
-		    wind.setText(windJ.get("speed").getAsString() + " m/s");
-		    cloud.setText(rootobj.get("clouds").getAsJsonObject().get("all").getAsString()+"%");
-		    pressure.setText(main.get("pressure").getAsString()+"hPA");
-		    humidity.setText(main.get("humidity").getAsString()+"%");
-		    
-		    //test.setText("Nazwa miasta " + rootobj.get("name").getAsString() + " pogoda "+ weatherr.get("main").getAsString() +  " temperatura to " + Integer.valueOf(temp).toString() + "C");
-    	}
-    	else if(choiceBox.getValue()=="Search by your location*") {
-    		String ip = null;
-    		try {
-				ip = getPublicIp();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    		
-    		System.out.println(ip);
+		    String city = rootobj.get("city").getAsString();
+		    city = polishFix(city);
+		    System.out.println(city);
+		    setWeather(city);
     	}
     	else {
     		alert("Choose something, pls....");
@@ -150,7 +121,7 @@ public class MainController implements Initializable {
     	tempLabel.setText("");
     	weatherLabel.setText("");
     	dateLabel.setText("");
-    	choiceBox.getItems().addAll("Search by city name", "Search by your location*");
+    	choiceBox.getItems().addAll("Search by city name", "Search by your location");
     }
     void alert(String text) {
     	Alert alert = new Alert(AlertType.INFORMATION);
@@ -177,5 +148,60 @@ public class MainController implements Initializable {
 				e.printStackTrace();
 			}
     		return ip;
+    }
+    String polishFix(String text) {
+    	text = text.replace("ł","l");
+    	return text;
+    }
+    void setWeather(String city) {
+    	String sURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+		sURL = new StringBuilder(sURL).append(city).toString();
+		sURL = new StringBuilder(sURL).append("&APPID=c51c62d65d9b1125d513f20cb5107341").toString();
+		System.out.println(sURL);
+		
+		JsonElement root = null;
+	    try {
+	    	URL url = new URL(sURL);
+	    	HttpURLConnection request = (HttpURLConnection) url.openConnection();
+			request.connect();
+			
+			JsonParser jp = new JsonParser();
+			root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			alert("Localization not found :(");
+			e.printStackTrace();
+		}
+
+	    //Convert the input stream to a json element
+	    JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object. 
+	    System.out.println(rootobj.toString());
+	    JsonObject main = rootobj.get("main").getAsJsonObject();
+	    JsonObject windJ = rootobj.get("wind").getAsJsonObject();
+	    JsonArray weather = rootobj.get("weather").getAsJsonArray();
+	    JsonObject weatherr = weather.get(0).getAsJsonObject();
+	    long sunriseUnix = rootobj.get("sys").getAsJsonObject().get("sunrise").getAsLong();
+	    long sunsetUnix = rootobj.get("sys").getAsJsonObject().get("sunset").getAsLong();
+	    long dateUnix = rootobj.get("dt").getAsLong();
+	    Date date = new Date ();
+	    date.setTime((long)sunriseUnix*1000);
+	    sunrise.setText(fixTime(date.toString()));
+	    date.setTime((long)sunsetUnix*1000);
+	    sunset.setText(fixTime(date.toString()));
+	    date.setTime((long)dateUnix*1000);
+	    dateLabel.setText("	" + fixTime(date.toString()));
+	    
+	    String imageUrl = "http://openweathermap.org/img/w/";
+	    imageUrl = new StringBuilder(imageUrl).append(weatherr.get("icon").getAsString()+ ".png").toString();
+	    icon.setImage(new Image(imageUrl));
+	    int temp = main.get("temp").getAsInt();
+	    temp = temp - 273;
+	    top.setText("Weather in " + rootobj.get("name").getAsString()+", "+rootobj.get("sys").getAsJsonObject().get("country").getAsString());
+	    tempLabel.setText(Integer.valueOf(temp).toString() + " C");
+	    weatherLabel.setText(weatherr.get("main").getAsString());
+	    wind.setText(windJ.get("speed").getAsString() + " m/s");
+	    cloud.setText(rootobj.get("clouds").getAsJsonObject().get("all").getAsString()+"%");
+	    pressure.setText(main.get("pressure").getAsString()+"hPA");
+	    humidity.setText(main.get("humidity").getAsString()+"%");
     }
 }
